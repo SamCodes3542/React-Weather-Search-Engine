f6837b075ae12ea19bb43cb37c5664488bc05f22
SamCodes3542 <106327181+SamCodes3542@users.noreply.github.com>
2022-12-25 13:04:21 -0600

    added axios and updated package for install in package.json
    Updated date and formatting as well as added css and icon for weather

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,132 @@
 import "./App.css";
-import React from "react";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherDate from "./WeatherDate";
 
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <h1>Georgia</h1>
-            <h2 className="weather">Temp</h2>
-            <ul className="currentWeatherConditions">
-              <li className="date">date</li>
-              <li className="time">time</li>
-              <li className="currentConditions">current</li>
-            </ul>
-            <ul className="weatherConditions">
-              <li>Humidity: </li>
-              <li>Wind: </li>
-            </ul>
-            <input
-              placeholder="Enter a City"
-              autoFocus="on"
-              className="input"
-            />
-            <input type="button" value="Search" className="button" />
-            <div className="row mt-3">
-                <div className="col-2">Mon</div>
-                <div className="col-2">Tue</div>
-                <div className="col-2">Wed</div>
-                <div className="col-2">Thurs</div>
-                <div className="col-2">Fri</div>
+export default function App() {
+  const [weatherData, setWeatherData] = useState({ loaded: false });
+
+  function showResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      loaded: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      feelsLike: Math.round(response.data.main.feels_like),
+      date: new Date(response.data.dt * 1000),
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      iconUrl: "http://openweathermap.org/img/wn/10d@2x.png",
+    });
+  }
+  if (weatherData.loaded) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="city"> {weatherData.city} </h1>
+              <img
+                src="http://openweathermap.org/img/wn/10d@2x.png"
+                className="icon"
+                alt="icon"
+              />
+              <h2 className="temperature">
+                {Math.round(weatherData.temperature)}
+              </h2>
+              <span className="unit">C</span>
+              <div className="text-capitalize">
+                <ul className="currentWeatherConditions">
+                  <li className="date">
+                    <WeatherDate date={weatherData.date} />
+                  </li>
+                  <li className="descrption">{weatherData.description}</li>
+                </ul>
+              </div>
+              <ul className="weatherConditions">
+                <li>
+                  Feels Like: {weatherData.feelsLike}
+                  <span className="float-left">c</span>{" "}
+                </li>
+                <li>Humidity: {weatherData.humidity}% </li>
+                <li>Wind: {Math.round(weatherData.wind)}km/h</li>
+              </ul>
+              <input
+                placeholder="Enter a City"
+                autoFocus="on"
+                className="formInput"
+              />
+              <input type="button" value="Search" className="button" />
+              <div className="row">
+                <div className="col-2">
+                  Mon
+                  <br />
+                  temp
+                  <br />☀
+                </div>
+                <div className="col-2">
+                  Tue
+                  <br />
+                  temp
+                  <br />☀
+                </div>
+                <div className="col-2">
+                  Wed
+                  <br />
+                  temp
+                  <br />☀
+                </div>
+                <div className="col-2">
+                  Thurs
+                  <br />
+                  temp
+                  <br />☀
+                </div>
+                <div className="col-2">
+                  Fri
+                  <br />
+                  temp
+                  <br />☀
+                </div>
               </div>
             </div>
           </div>
         </div>
         <footer>
-          Open-Source coded on
-          <a href="">Github</a>
-          By
-          <a href=""> Samantha Montgomery</a>
-          hosted on
-          <a href="">Netlify</a>
+          Open-Source coded on {""}
+          <a
+            className="gitLink"
+            href="https://github.com/SamCodes3542/VIRTUAL-GAMER-WEATHER-APP-PROJECT"
+            target="_blank rel=noopener noreferrer"
+          >
+            Github
+          </a>{" "}
+          By{" "}
+          <a
+            className="linkedIn"
+            href="https://www.linkedin.com/in/samantha-montgomery-70a68323a/"
+            target="_blank rel=noopener noreferrer"
+          >
+            Samantha Montgomery
+          </a>{" "}
+          hosted on{" "}
+          <a
+            className="netlifyLink"
+            href="netlify.com"
+            target="_blank rel=noopener noreferrer"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
-  );
-}
+    );
+  } else {
+    let city = "Paris";
+    const apiKey = "094b50c9907d04014c22a077f5e1062a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showResponse);
 
-export default App;
+    return "Loading...";
+  }
+}
---
+++ my-app/src/WeatherDate.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function WeatherDate(props) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Teusday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 0) {
+    hours = `0${hours}`;
+  }
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
+}
